36ff1fe5a33176cba832cae6d9d0098fb6a3e82c
Freddie <pwed@users.noreply.github.com>
2022-03-01 23:09:56 +1100

    fixing jest again

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: [ 'js'],
+  moduleFileExtensions: ['js'],
+  roots: [
+    '<rootDir>/lib/static-site/test',
+    // '<rootDir>/lib/bastion/test',
+  ],
 };
