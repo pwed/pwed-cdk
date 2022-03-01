9844020b0a244b8d60441663e397cf77c3b896f9
Freddie <pwed@users.noreply.github.com>
2022-03-01 23:25:17 +1100

    change to json jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js'],
-  roots: [
-    '<rootDir>/lib/static-site/test',
-    // '<rootDir>/lib/bastion/test',
-  ],
-};
