f0a8c9b8c7bfa3e6d3eeaf8e6c8735515103a126
Freddie <pwed@users.noreply.github.com>
2022-03-02 09:21:07 +1100

    auto push

--- .projenrc.js
+++ .projenrc.js
@@ -14,5 +14,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  // Publishing
+  releaseToNpm: true,
+  npmTokenSecret: 'NPM_ACCESS_TOKEN',
 });
 project.synth();
