54a92b5fc11f4b1c61ac897709198d66f737fa46
Freddie <pwed@users.noreply.github.com>
2022-03-02 01:22:02 +1100

    chore: project created with projen

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pwed_bastion = require("./lib/bastion");
+exports.pwed_static_site = require("./lib/static-site");
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLGdEQUE4QztBQUM5Qyx3REFBc0QiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgKiBhcyBwd2VkX2Jhc3Rpb24gZnJvbSAnLi9saWIvYmFzdGlvbic7XG5leHBvcnQgKiBhcyBwd2VkX3N0YXRpY19zaXRlIGZyb20gJy4vbGliL3N0YXRpYy1zaXRlJztcbiJdfQ==
