e87bd7e758f970fc613f6b376b2269d4dbc1d6ce
Fred  Stoddart <fred.stoddart@ac3.com.au>
2022-06-03 16:24:16 +1000

    fixing lambda asset missing from python build

--- src/lib/ttl/index.ts
+++ src/lib/ttl/index.ts
@@ -1,4 +1,3 @@
-import { join } from 'path';
 import {
   Duration,
   aws_lambda,
@@ -25,7 +24,49 @@ export class Ttl extends Construct {
 
     const ttlLambda = new aws_lambda.Function(this, 'TtlFunction', {
       runtime: aws_lambda.Runtime.PYTHON_3_9,
-      code: aws_lambda.Code.fromAsset(join(__dirname, 'lambda')),
+      code: aws_lambda.Code.fromInline(`#!/bin/env python
+import boto3
+import os
+import time
+import logging
+
+log = logging.Logger("logger")
+log.setLevel("INFO")
+
+def handler(event, context):
+    print(event)
+
+    stack_name = os.environ['STACK_NAME']
+    ttl = int(os.environ['TTL'])
+    cfn = boto3.client('cloudformation')
+
+    print(
+        f"""
+        StackName: {stack_name}
+        TTL:       {ttl}
+        """
+    )
+
+    stack = cfn.describe_stacks(StackName=stack_name)
+    creation_time = stack['Stacks'][0]['CreationTime']
+    creation_time_epoch = int(time.mktime(creation_time.timetuple()))
+
+    now = time.time()
+    delta = now - creation_time_epoch
+    print(
+        f"""
+        Now:           {now}
+        Creation Time: {creation_time}
+        Delta:         {delta}
+        """
+    )
+    if delta > ttl:
+        print(f"deleting stack: {stack_name}")
+        cfn.delete_stack(StackName=stack_name)
+        return
+
+    print(f"not deleting stack yet, {delta} seconds remaining")
+    return`),
       handler: 'ttl.handler',
       environment: {
         TTL: ttl.toSeconds().toString(),
